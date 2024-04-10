be90073164a461d01ebac80bb247d1a5d581e6c8
RLCastaldy <rlcastaldy@gmail.com>
2024-04-10 18:40:48 -0500

    added mutations and queries as well as fixed issues with resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,23 +1,52 @@
-const { createUser, getSingleUser, saveBook, deleteBook, login } = require('../controllers/user-controller');
+const { User } = require('../models');
 
 const resolvers = {
-    Query: {
-      me: getSingleUser,
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return await User.findById(context.user._id);
+      } else {
+        throw new Error('Authentication required to view user data');
+      }
     },
-    Mutation: {
-      addUser: createUser,
-      login: login,
-      saveBook: async (parent, args, context) => {
-        if (context.user) {
-          // User is authenticated, proceed with saving the book
-          return saveBook(parent, args, context);
-        } else {
-          // User is not authenticated, throw an error or handle accordingly
-          throw new Error('Authentication required to save a book');
-        }
-      },
-      removeBook: deleteBook,
+  },
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new Error('Incorrect email or password');
+      }
+
+      const correctPassword = await user.isCorrectPassword(password);
+      if (!correctPassword) {
+        throw new Error('Incorrect email or password');
+      }
+
+      const token = signToken(user);
+      return { token, user };
     },
-  };
-  
-  module.exports = resolvers;
+    addUser: async (parent, args) => {
+      const user = await createUser(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, { input }, context) => {
+      if (context.user) {
+        const updatedUser = await saveBook(context.user._id, input);
+        return updatedUser;
+      } else {
+        throw new Error('Authentication required to save a book');
+      }
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await deleteBook(context.user._id, bookId);
+        return updatedUser;
+      } else {
+        throw new Error('Authentication required to remove a book');
+      }
+    },
+  },
+};
+
+module.exports = resolvers;
