eb6c5c71554344b498c00c0ca0105de71a7e5793
RLCastaldy <rlcastaldy@gmail.com>
2024-04-15 16:29:10 -0500

    fixed errors caused in debugging

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
-require('dotenv').config(); // Load environment variables from .env file
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, {}); // Use the MONGODB_URI environment variable
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
+});
 
 module.exports = mongoose.connection;
